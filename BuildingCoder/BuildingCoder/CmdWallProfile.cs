2fcb581f796ff4e0b237d7f3c49e360d6cbfabdc
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2015-04-10 10:27:24 +0200

    integrated Alexander Ignatovich ai CmdWallProfile - first attempt.cs

--- BuildingCoder/BuildingCoder/CmdWallProfile.cs
+++ BuildingCoder/BuildingCoder/CmdWallProfile.cs
@@ -12,6 +12,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using Autodesk.Revit.ApplicationServices;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
@@ -332,18 +333,129 @@ namespace BuildingCoder
       return Result.Succeeded;
     }
 
-    public Result Execute(
+    /// <summary>
+    /// Improved implementation by Alexander Ignatovich
+    /// supporting curved wall with curved window, 
+    /// published April 10, 2015:
+    /// </summary>
+    public Result Execute3(
       ExternalCommandData commandData,
       ref string message,
       ElementSet elements )
     {
+      UIApplication uiapp = commandData.Application;
+      UIDocument uidoc = uiapp.ActiveUIDocument;
+      Application app = uiapp.Application;
+      Document doc = uidoc.Document;
+      View view = doc.ActiveView;
+
+      Autodesk.Revit.Creation.Application creapp
+        = app.Create;
+
+      Autodesk.Revit.Creation.Document credoc
+        = doc.Create;
+
+      Reference r = uidoc.Selection.PickObject(
+        ObjectType.Element, "Select a wall" );
+
+      Element e = uidoc.Document.GetElement( r );
+
+      Wall wall = e as Wall;
+      
+      if( wall == null )
+      {
+        return Result.Cancelled;
+      }
+
+      using( Transaction tx = new Transaction( doc ) )
+      {
+        tx.Start( "Wall Profile" );
+
+        // Get the external wall face for the profile
+        // a little bit simpler than in the last realization
+
+        Reference sideFaceReference 
+          = HostObjectUtils.GetSideFaces( 
+            wall, ShellLayerType.Exterior )
+              .First();
+
+        Face face = wall.GetGeometryObjectFromReference( 
+          sideFaceReference ) as Face;
+
+        // The normal of the wall external face.
+
+        XYZ normal = wall.Orientation;
+
+        // Offset curve copies five feet for visibility.
+
+        Transform offset = Transform.CreateTranslation(
+          5 * normal );
+
+        // If the curve loop direction is counter-
+        // clockwise, change its color to RED.
+
+        Color colorRed = new Color( 255, 0, 0 );
+
+        // Get edge loops as curve loops.
+
+        IList<CurveLoop> curveLoops
+          = face.GetEdgesAsCurveLoops();
+
+        foreach( var curveLoop in curveLoops )
+        {
+          CurveArray curves = creapp.NewCurveArray();
+
+          foreach( Curve curve in curveLoop )
+            curves.Append( curve.CreateTransformed( offset ) );
+
+          // Create model lines for an curve loop.
+
+          Plane plane = creapp.NewPlane( curves );
+
+          SketchPlane sketchPlane
+            = SketchPlane.Create( doc, plane );
+
+          ModelCurveArray curveElements
+            = credoc.NewModelCurveArray( curves,
+              sketchPlane );
+
+          if( curveLoop.IsCounterclockwise( normal ) )
+          {
+            foreach( ModelCurve mcurve in curveElements )
+            {
+              OverrideGraphicSettings overrides
+                = view.GetElementOverrides(
+                  mcurve.Id );
+
+              overrides.SetProjectionLineColor(
+                colorRed );
+
+              view.SetElementOverrides(
+                mcurve.Id, overrides );
+            }
+          }
+        }
+        tx.Commit();
+      }
+      return Result.Succeeded;
+    }
+
+    public Result Execute(
+      ExternalCommandData cd,
+      ref string msg,
+      ElementSet els )
+    {
       // Choose which implementation to use.
 
-      bool use_execute_2 = true;
+      int use_execute_nr = 3;
 
-      return use_execute_2
-        ? Execute2( commandData, ref message, elements )
-        : Execute1( commandData, ref message, elements );
+      switch( use_execute_nr )
+      {
+        case 1: return Execute1( cd, ref msg, els );
+        case 2: return Execute2( cd, ref msg, els );
+        case 3: return Execute3( cd, ref msg, els );
+      }
+      return Result.Failed;
     }
   }
 }
