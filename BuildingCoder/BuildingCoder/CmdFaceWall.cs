f18ce655a38f84f46fec5b5d823a67dc459a4b67
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2015-05-27 08:52:08 +0200

    whitespace

--- BuildingCoder/BuildingCoder/CmdFaceWall.cs
+++ BuildingCoder/BuildingCoder/CmdFaceWall.cs
@@ -27,198 +27,198 @@ namespace BuildingCoder
   [Transaction( TransactionMode.Manual )]
   class CmdFaceWall : IExternalCommand
   {
-  const string _conceptual_mass_template_path
-    = "C:/ProgramData/Autodesk/RVT 2015"
-    + "/Family Templates/English/Conceptual Mass"
-    + "/Metric Mass.rft";
+    const string _conceptual_mass_template_path
+      = "C:/ProgramData/Autodesk/RVT 2015"
+      + "/Family Templates/English/Conceptual Mass"
+      + "/Metric Mass.rft";
 
-  const string _family_name = "TestFamily";
+    const string _family_name = "TestFamily";
 
-  const string _family_path = "C:/" + _family_name + ".rfa";
+    const string _family_path = "C:/" + _family_name + ".rfa";
 
-  static ModelCurve MakeLine(
-    Document doc,
-    XYZ p,
-    XYZ q )
-  {
-    // Create plane by the points
-
-    Line line = Line.CreateBound( p, q );
-    XYZ norm = p.CrossProduct( q );
-    if( norm.GetLength() == 0 ) { norm = XYZ.BasisZ; }
-    Plane plane = new Plane( norm, q );
-
-    SketchPlane skplane = SketchPlane.Create(
-      doc, plane );
-
-    // Create line
-
-    return doc.FamilyCreate.NewModelCurve(
-      line, skplane );
-  }
-
-  /// <summary>
-  /// Create an extrusion form in the given
-  /// conceptual mass family document.
-  /// </summary>
-  static void CreateMassExtrusion(
-    Document doc )
-  {
-    using( Transaction tx = new Transaction( doc ) )
+    static ModelCurve MakeLine(
+      Document doc,
+      XYZ p,
+      XYZ q )
     {
-      tx.Start( "Create Mass" );
+      // Create plane by the points
 
-      // Create profile
+      Line line = Line.CreateBound( p, q );
+      XYZ norm = p.CrossProduct( q );
+      if( norm.GetLength() == 0 ) { norm = XYZ.BasisZ; }
+      Plane plane = new Plane( norm, q );
 
-      ReferenceArray refar = new ReferenceArray();
+      SketchPlane skplane = SketchPlane.Create(
+        doc, plane );
 
-      XYZ[] pts = new XYZ[] {
+      // Create line
+
+      return doc.FamilyCreate.NewModelCurve(
+        line, skplane );
+    }
+
+    /// <summary>
+    /// Create an extrusion form in the given
+    /// conceptual mass family document.
+    /// </summary>
+    static void CreateMassExtrusion(
+      Document doc )
+    {
+      using( Transaction tx = new Transaction( doc ) )
+      {
+        tx.Start( "Create Mass" );
+
+        // Create profile
+
+        ReferenceArray refar = new ReferenceArray();
+
+        XYZ[] pts = new XYZ[] {
         new XYZ( -10, -10, 0 ), 
         new XYZ( +10, -10, 0 ), 
         new XYZ( +10, +10, 0 ), 
         new XYZ( -10, +10, 0 ) };
 
-      int j, n = pts.Length;
+        int j, n = pts.Length;
 
-      for( int i = 0; i < n; ++i )
-      {
-        j = i + 1;
-
-        if( j >= n ) { j = 0; }
-
-        // The Creator.CreateModelLine method creates 
-        // pretty arbitrary sketch planes, which causes
-        // the NewExtrusionForm method to fail, saying 
-        // "Cannot create extrude form."
-
-        //ModelCurve c = Creator.CreateModelLine( doc, pts[i], pts[j] );
-
-        ModelCurve c = MakeLine( doc, pts[i], pts[j] );
-
-        refar.Append( c.GeometryCurve.Reference );
-      }
-
-      //doc.Regenerate();
-
-      // The extrusion form direction and length.
-      // The direction must be perpendicular to the 
-      // plane determined by profile. The length 
-      // must be non-zero.
-
-      XYZ direction = new XYZ( /*-6*/ 0, 0, 20 );
-
-      Form form = doc.FamilyCreate.NewExtrusionForm( // Cannot create extrude form.
-        true, refar, direction );
-
-      tx.Commit();
-    }
-  }
-
-  static void CreateFaceWalls(
-    Document doc )
-  {
-    Application app = doc.Application;
-
-    Document massDoc = app.NewFamilyDocument(
-      _conceptual_mass_template_path );
-
-    CreateMassExtrusion( massDoc );
-
-    //if( File.Exists( _family_path ) )
-    //  File.Delete( _family_path );
-
-    SaveAsOptions opt = new SaveAsOptions();
-    opt.OverwriteExistingFile = true;
-
-    massDoc.SaveAs( _family_path, opt );
-
-    using( Transaction tx = new Transaction( doc ) )
-    {
-      tx.Start( "Create FaceWall" );
-
-      if( !doc.LoadFamily( _family_path ) )
-        throw new Exception( "DID NOT LOAD FAMILY" );
-
-      Family family = new FilteredElementCollector( doc )
-        .OfClass( typeof( Family ) )
-        .Where<Element>( x => x.Name.Equals( _family_name ) )
-        .Cast<Family>()
-        .FirstOrDefault();
-
-      FamilySymbol fs = doc.GetElement(
-        family.GetFamilySymbolIds().First<ElementId>() )
-          as FamilySymbol;
-
-      // Create a family instance
-
-      Level level = doc.ActiveView.GenLevel;
-
-      FamilyInstance fi = doc.Create.NewFamilyInstance(
-        XYZ.Zero, fs, level, StructuralType.NonStructural );
-
-      doc.Regenerate(); // required to generate the geometry!
-
-      // Determine wall type.
-
-      WallType wallType = new FilteredElementCollector( doc )
-        .OfClass( typeof( WallType ) )
-        .Cast<WallType>()
-        .Where<WallType>( x => FaceWall.IsWallTypeValidForFaceWall( doc, x.Id ) )
-        .FirstOrDefault();
-
-      // Retrieve mass element geometry.
-
-      Options options = app.Create.NewGeometryOptions();
-      options.ComputeReferences = true;
-
-      //options.View = doc.ActiveView; // conceptual mass is not visible in default view
-
-      GeometryElement geo = fi.get_Geometry( options );
-
-      // Create a sloped wall from the geometry.
-
-      foreach( GeometryObject obj in geo )
-      {
-        Solid solid = obj as Solid;
-
-        if( null != solid )
+        for( int i = 0; i < n; ++i )
         {
-          foreach( Face f in solid.Faces )
+          j = i + 1;
+
+          if( j >= n ) { j = 0; }
+
+          // The Creator.CreateModelLine method creates 
+          // pretty arbitrary sketch planes, which causes
+          // the NewExtrusionForm method to fail, saying 
+          // "Cannot create extrude form."
+
+          //ModelCurve c = Creator.CreateModelLine( doc, pts[i], pts[j] );
+
+          ModelCurve c = MakeLine( doc, pts[i], pts[j] );
+
+          refar.Append( c.GeometryCurve.Reference );
+        }
+
+        //doc.Regenerate();
+
+        // The extrusion form direction and length.
+        // The direction must be perpendicular to the 
+        // plane determined by profile. The length 
+        // must be non-zero.
+
+        XYZ direction = new XYZ( /*-6*/ 0, 0, 20 );
+
+        Form form = doc.FamilyCreate.NewExtrusionForm( // Cannot create extrude form.
+          true, refar, direction );
+
+        tx.Commit();
+      }
+    }
+
+    static void CreateFaceWalls(
+      Document doc )
+    {
+      Application app = doc.Application;
+
+      Document massDoc = app.NewFamilyDocument(
+        _conceptual_mass_template_path );
+
+      CreateMassExtrusion( massDoc );
+
+      //if( File.Exists( _family_path ) )
+      //  File.Delete( _family_path );
+
+      SaveAsOptions opt = new SaveAsOptions();
+      opt.OverwriteExistingFile = true;
+
+      massDoc.SaveAs( _family_path, opt );
+
+      using( Transaction tx = new Transaction( doc ) )
+      {
+        tx.Start( "Create FaceWall" );
+
+        if( !doc.LoadFamily( _family_path ) )
+          throw new Exception( "DID NOT LOAD FAMILY" );
+
+        Family family = new FilteredElementCollector( doc )
+          .OfClass( typeof( Family ) )
+          .Where<Element>( x => x.Name.Equals( _family_name ) )
+          .Cast<Family>()
+          .FirstOrDefault();
+
+        FamilySymbol fs = doc.GetElement(
+          family.GetFamilySymbolIds().First<ElementId>() )
+            as FamilySymbol;
+
+        // Create a family instance
+
+        Level level = doc.ActiveView.GenLevel;
+
+        FamilyInstance fi = doc.Create.NewFamilyInstance(
+          XYZ.Zero, fs, level, StructuralType.NonStructural );
+
+        doc.Regenerate(); // required to generate the geometry!
+
+        // Determine wall type.
+
+        WallType wallType = new FilteredElementCollector( doc )
+          .OfClass( typeof( WallType ) )
+          .Cast<WallType>()
+          .Where<WallType>( x => FaceWall.IsWallTypeValidForFaceWall( doc, x.Id ) )
+          .FirstOrDefault();
+
+        // Retrieve mass element geometry.
+
+        Options options = app.Create.NewGeometryOptions();
+        options.ComputeReferences = true;
+
+        //options.View = doc.ActiveView; // conceptual mass is not visible in default view
+
+        GeometryElement geo = fi.get_Geometry( options );
+
+        // Create a sloped wall from the geometry.
+
+        foreach( GeometryObject obj in geo )
+        {
+          Solid solid = obj as Solid;
+
+          if( null != solid )
           {
-            Debug.Assert( null != f.Reference,
-              "we asked for references, didn't we?" );
-
-            PlanarFace pf = f as PlanarFace;
-
-            if( null != pf )
+            foreach( Face f in solid.Faces )
             {
-              XYZ v = pf.Normal;
+              Debug.Assert( null != f.Reference,
+                "we asked for references, didn't we?" );
 
-              // Errors:
-              //
-              // Could not create a face wall.
-              //
-              // Caused by using ActiveView.Level 
-              // instead of ActiveView.GenLevel.
-              //
-              // This reference cannot be applied to a face wall.
-              //
-              // Caused by using this on a horizontal face.
+              PlanarFace pf = f as PlanarFace;
 
-              if( !Util.IsVertical( v ) )
+              if( null != pf )
               {
-                FaceWall.Create(
-                  doc, wallType.Id,
-                  WallLocationLine.CoreCenterline,
-                  f.Reference );
+                XYZ v = pf.Normal;
+
+                // Errors:
+                //
+                // Could not create a face wall.
+                //
+                // Caused by using ActiveView.Level 
+                // instead of ActiveView.GenLevel.
+                //
+                // This reference cannot be applied to a face wall.
+                //
+                // Caused by using this on a horizontal face.
+
+                if( !Util.IsVertical( v ) )
+                {
+                  FaceWall.Create(
+                    doc, wallType.Id,
+                    WallLocationLine.CoreCenterline,
+                    f.Reference );
+                }
               }
             }
           }
         }
+        tx.Commit();
       }
-      tx.Commit();
     }
-  }
 
     public Result Execute(
       ExternalCommandData commandData,
