2bcac845a93bf811b6e77ebd132c6591593125d0
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2016-04-05 19:56:10 +0200

    implemented DistinguishRooms

--- BuildingCoder/BuildingCoder/CmdListAllRooms.cs
+++ BuildingCoder/BuildingCoder/CmdListAllRooms.cs
@@ -13,13 +13,13 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Text;
 using Autodesk.Revit.ApplicationServices;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.DB.Architecture;
 using Autodesk.Revit.UI;
 using BoundarySegment = Autodesk.Revit.DB.BoundarySegment;
-using System.Text;
 #endregion // Namespaces
 
 namespace BuildingCoder
