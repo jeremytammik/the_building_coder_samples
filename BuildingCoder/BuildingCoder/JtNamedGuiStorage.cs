1171d81dc9e8a3732a9cf2379ebf71d8bdeee2cb
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2016-04-11 07:59:05 +0200

    implemeneted JtNamedGuiStorage skeleton

---
+++ BuildingCoder/BuildingCoder/JtNamedGuiStorage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BuildingCoder
+{
+  class JtNamedGuiStorage
+  {
+    static bool Set( string name, Guid guid )
+    {
+      bool rc = false;
+      return rc;
+    }
+
+    static Guid Get( string name, bool create = true )
+    {
+      return new Guid();
+    }
+  }
+}
