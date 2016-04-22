4e211d4a4a083bf3f80deb60e441c64628039a8a
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2016-04-22 18:36:39 +0200

    minor cleanup

--- BuildingCoder/BuildingCoder/CmdDimensionInstanceOrigin.cs
+++ BuildingCoder/BuildingCoder/CmdDimensionInstanceOrigin.cs
@@ -29,32 +29,11 @@ namespace BuildingCoder
   // in family instance and retrieve dimensioning 
   // reference to it]
 
-  //
-  // (C) Copyright 2003-2007 by Autodesk, Inc.
-  //
-  // Permission to use, copy, modify, and distribute this software in
-  // object code form for any purpose and without fee is hereby granted,
-  // provided that the above copyright notice appears in all copies and
-  // that both that copyright notice and the limited warranty and
-  // restricted rights notice below appear in all supporting
-  // documentation.
-  //
-  // AUTODESK PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS.
-  // AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
-  // MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE. AUTODESK, INC.
-  // DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
-  // UNINTERRUPTED OR ERROR FREE.
-  //
-  // Use, duplication, or disclosure by the U.S. Government is subject to
-  // restrictions set forth in FAR 52.227-19 (Commercial Computer
-  // Software - Restricted Rights) and DFAR 252.227-7013(c)(1)(ii)
-  // (Rights in Technical Data and Computer Software), as applicable.
-  //
-
   /// <summary>
-  /// get element's id and type information and its supported information.
+  /// Get element's id and type information 
+  /// and its supported information.
   /// </summary>
-  [Autodesk.Revit.Attributes.Transaction( Autodesk.Revit.Attributes.TransactionMode.Manual )]
+  [Transaction( TransactionMode.Manual )]
   public class Command : IExternalCommand
   {
     public Result Execute(
@@ -241,10 +220,10 @@ namespace BuildingCoder
       }
     }
 
-    private Autodesk.Revit.UI.UIApplication m_uiApp;
-    private Autodesk.Revit.UI.UIDocument m_uiDoc;
-    private Autodesk.Revit.ApplicationServices.Application m_app;
-    private Autodesk.Revit.DB.Document m_doc;
+    private UIApplication m_uiApp;
+    private UIDocument m_uiDoc;
+    private Application m_app;
+    private Document m_doc;
     private TextWriter m_writer;
   }
   #endregion // Scott's sample code from SPR #201483
