aa3e547547cdf27c701864ec4262ce70f14e9267
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2016-08-19 09:16:01 +0200

    cleanup

--- BuildingCoder/BuildingCoder/CmdRemoveDwfLinks.cs
+++ BuildingCoder/BuildingCoder/CmdRemoveDwfLinks.cs
@@ -12,7 +12,6 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using Autodesk.Revit.ApplicationServices;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
