c219d240e4d59bbb60103c20ab205df6b0d81b46
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2016-12-24 15:16:11 +0100

    implemented JtElementIdExtensionMethods

--- BuildingCoder/BuildingCoder/Util.cs
+++ BuildingCoder/BuildingCoder/Util.cs
@@ -1426,6 +1426,24 @@ namespace BuildingCoder
     }
   }
 
+  public static class JtElementIdExtensionMethods
+  {
+    /// <summary>
+    /// Predicate returning true for invalid element ids.
+    /// </summary>
+    public static bool IsInValid( this ElementId id )
+    {
+      return ElementId.InvalidElementId == id;
+    }
+    /// <summary>
+    /// Predicate returning true for valid element ids.
+    /// </summary>
+    public static bool IsValid( this ElementId id )
+    {
+      return !IsInValid( id );
+    }
+  }
+
   public static class JtBoundingBoxXyzExtensionMethods
   {
     /// <summary>
