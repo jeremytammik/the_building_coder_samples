54a940eef88dae1201f9a9771c651267211de4bf
Jeremy Tammik <jeremy.tammik@autodesk.com>
2017-06-13 10:59:35 +0200

    added AlignHorizontalAndVerticalGridLine

--- BuildingCoder/BuildingCoder/CmdSetGridEndpoint.cs
+++ BuildingCoder/BuildingCoder/CmdSetGridEndpoint.cs
@@ -2,7 +2,7 @@
 //
 // CmdSetGridEndpoint.cs - move selected grid endpoints in Y direction using SetCurveInView
 //
-// Copyright (C) 2017 by  Ryuji Ogasawara and Jeremy Tammik, Autodesk Inc. All rights reserved.
+// Copyright (C) 2017 by Ryuji Ogasawara and Jeremy Tammik, Autodesk Inc. All rights reserved.
 //
 // Keywords: The Building Coder Revit API C# .NET add-in.
 //
@@ -15,6 +15,7 @@ using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
+using System;
 using System.Collections.Generic;
 #endregion // Namespaces
 
@@ -23,6 +24,67 @@ namespace BuildingCoder
   [Transaction( TransactionMode.Manual )]
   public class CmdSetGridEndpoint : IExternalCommand
   {
+    /// <summary>
+    /// Align the given grid horizontally or vertically 
+    /// if it is very slightly off axis, by Fair59 in
+    /// https://forums.autodesk.com/t5/revit-api-forum/grids-off-axis/m-p/7129065
+    /// </summary>
+    void AlignHorizontalAndVerticalGridLine(
+      Grid grid )
+    {
+      //Grid grid = doc.GetElement( 
+      //  sel.GetElementIds().FirstOrDefault() ) as Grid;
+
+      Document doc = grid.Document;
+
+      XYZ direction = grid.Curve
+        .GetEndPoint( 1 )
+        .Subtract( grid.Curve.GetEndPoint( 0 ) )
+        .Normalize();
+
+      double distance2hor = direction.DotProduct( XYZ.BasisY );
+      double distance2vert = direction.DotProduct( XYZ.BasisX );
+      double angle = 0;
+
+      // Maybe use another criterium then <0.0001
+
+      double max_distance = 0.0001;
+
+      if( Math.Abs( distance2hor ) < max_distance )
+      {
+        XYZ vector = direction.X < 0 
+          ? direction.Negate() 
+          : direction;
+
+        angle = Math.Asin( -vector.Y );
+      }
+
+      if( Math.Abs( distance2vert ) < max_distance )
+      {
+        XYZ vector = direction.Y < 0 
+          ? direction.Negate() 
+          : direction;
+
+        angle = Math.Asin( vector.X );
+      }
+
+      if( angle.CompareTo( 0 ) != 0 )
+      {
+        using( Transaction t = new Transaction( doc ) )
+        {
+          t.Start( "correctGrid" );
+
+          ElementTransformUtils.RotateElement( doc, 
+            grid.Id, 
+            Line.CreateBound( grid.Curve.GetEndPoint( 0 ), 
+              grid.Curve.GetEndPoint( 0 ).Add( XYZ.BasisZ ) ), 
+            angle );
+
+          t.Commit();
+        }
+      }
+    }
+
     public Result Execute(
       ExternalCommandData commandData,
       ref string message,
