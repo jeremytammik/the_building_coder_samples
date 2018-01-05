b8d0e5120fa7f8db2c05d8d3f0f414193134537b
Jeremy Tammik <jeremy.tammik@autodesk.com>
2018-01-05 23:09:13 +0100

    incremented copyright year to 2018

--- BuildingCoder/BuildingCoder/Properties/AssemblyInfo.cs
+++ BuildingCoder/BuildingCoder/Properties/AssemblyInfo.cs
@@ -205,7 +205,7 @@ using System.Runtime.InteropServices;
 // 2017-12-05 2018.0.134.10 added CreateFaceWallsAndMassFloors for case 13663566 [APIによるマス床の生成方法 -- How to generate mass floor using API]
 // 2017-12-20 2018.0.134.11 implemented Util.IsCollinear
 // 2017-12-20 2018.0.134.11 added Miro's SetInstanceParamVaryBetweenGroupsBehaviour
-// 2018-01-05 2018.0.134.12 incremented copyright year
+// 2018-01-05 2018.0.134.12 incremented copyright year to 2018
 //
 [assembly: AssemblyVersion( "2018.0.134.12" )]
 [assembly: AssemblyFileVersion( "2018.0.134.12" )]
