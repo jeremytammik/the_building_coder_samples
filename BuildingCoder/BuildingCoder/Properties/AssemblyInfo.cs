ef6a4dc7a2412d503d265ad5104a3ed24a130559
Jeremy Tammik <jeremy.tammik@autodesk.com>
2018-03-05 11:13:40 +0100

    updated CmdIntersectJunctionBox

--- BuildingCoder/BuildingCoder/Properties/AssemblyInfo.cs
+++ BuildingCoder/BuildingCoder/Properties/AssemblyInfo.cs
@@ -219,6 +219,7 @@ using System.Runtime.InteropServices;
 // 2018-02-27 2018.0.136.6 tested and fixed ToColorParameterValue
 // 2018-03-01 2018.0.136.7 added MepSystemSearch from https://github.com/geoffoverfield/RevitAPI_SystemSearch for https://forums.autodesk.com/t5/revit-api-forum/connector-neighbours/m-p/7816952
 // 2018-03-04 2018.0.137.0 implemented CmdIntersectJunctionBox
+// 2018-03-04 2018.0.137.0 updated CmdIntersectJunctionBox
 //
 [assembly: AssemblyVersion( "2018.0.137.0" )]
 [assembly: AssemblyFileVersion( "2018.0.137.0" )]
