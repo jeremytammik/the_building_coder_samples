eaa4500e7457632225f7b54fbfeca66076ef2bb0
Jeremy Tammik <jeremy.tammik@autodesk.com>
2018-05-23 14:36:01 +0200

    implemented GetBoundingBoxAroundAllWalls

--- BuildingCoder/BuildingCoder/CmdExteriorWalls.cs
+++ BuildingCoder/BuildingCoder/CmdExteriorWalls.cs
@@ -2,20 +2,20 @@
 //
 // CmdExteriorWalls.cs - retrieve all parameter values for all elements of the given categories
 //
-// Copyright (C) 2018 Jeremy Tammik, Autodesk Inc. All rights reserved.
+// Copyright (C) 2018 Feng Wang and Jeremy Tammik, Autodesk Inc. All rights reserved.
 //
 // Keywords: The Building Coder Revit API C# .NET add-in.
 //
 #endregion // Header
 
 #region Namespaces
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
-using Autodesk.Revit.UI;
 using Autodesk.Revit.DB.Architecture;
-using System.Linq;
-using System;
+using Autodesk.Revit.UI;
 #endregion // Namespaces
 
 namespace BuildingCoder
@@ -23,12 +23,30 @@ namespace BuildingCoder
   [Transaction( TransactionMode.ReadOnly )]
   class CmdExteriorWalls : IExternalCommand
   {
+    static BoundingBoxXYZ GetWallBoundingBoxAroundAllWalls( 
+      Document doc,
+      View view = null )
+    {
+      // Default constructor creates cube from -100 to 100
+
+      BoundingBoxXYZ bb = new BoundingBoxXYZ();
+
+      FilteredElementCollector walls
+        = new FilteredElementCollector( doc )
+          .OfClass( typeof( Wall ) );
+
+      foreach( Wall wall in walls )
+      {
+        bb.ExpandToContain( 
+          wall.get_BoundingBox( 
+            view ) );
+      }
+      return bb;
+    }
+
     /// <summary>
-    /// 过滤出需要的墙体
+    /// 过滤出需要的墙体 -- Filter out the required walls
     /// </summary>
-    /// <param name="doc"></param>
-    /// <param name="room"></param>
-    /// <returns></returns>
     static List<ElementId> DetermineAdjacentElementLengthsAndWallAreas(
       Document doc,
       Room room )
@@ -36,9 +54,10 @@ namespace BuildingCoder
       List<ElementId> elementIds = new List<ElementId>();
 
       IList<IList<BoundarySegment>> boundaries
-        = room.GetBoundarySegments( new SpatialElementBoundaryOptions() );
+        = room.GetBoundarySegments( 
+          new SpatialElementBoundaryOptions() );
 
-      int n = boundaries.Count;//.Size;
+      int n = boundaries.Count;
 
       int iBoundary = 0, iSegment;
 
@@ -49,8 +68,10 @@ namespace BuildingCoder
         foreach( BoundarySegment s in b )
         {
           ++iSegment;
-          Element neighbour = doc.GetElement( s.ElementId );
-          Curve curve = s.GetCurve();//.Curve;
+          Element neighbour = doc.GetElement(
+            s.ElementId );
+
+          Curve curve = s.GetCurve();
           double length = curve.Length;
 
           if( neighbour is Wall )
@@ -76,10 +97,12 @@ namespace BuildingCoder
 
     /// <summary>
     /// 获取当前模型指定视图内的所有最外层的墙体
+    /// Get all the outermost walls in the 
+    /// specified view of the current model
     /// </summary>
     /// <param name="doc"></param>
-    /// <param name="view">视图,默认是当前激活的视图</param>
-    /// <returns></returns>
+    /// <param name="view">视图,默认是当前激活的视图 
+    /// View, default is currently active view</param>
     public static List<ElementId> GetOutermostWalls( 
       Document doc, 
       View view = null )
@@ -146,29 +169,41 @@ namespace BuildingCoder
       curves.Append( line3 );
       curves.Append( line4 );
 
-      using( TransactionGroup group = new TransactionGroup( doc ) )
+      using( TransactionGroup group 
+        = new TransactionGroup( doc ) )
       {
         Room newRoom = null;
         RoomTag tag1 = null;
 
-        group.Start( "find outermost walls" );
-        using( Transaction transaction = new Transaction( 
-          doc, "createNewRoomBoundaryLines" ) )
-        {
-          transaction.Start();
-          if( view == null )
-            view = doc.ActiveView;
-          SketchPlane sketchPlane = SketchPlane.Create( doc, view.GenLevel.Id );
+        group.Start( "Find Outermost Walls" );
 
-          ModelCurveArray modelCaRoomBoundaryLines = doc.Create.NewRoomBoundaryLines( sketchPlane, curves, view );
+        using( Transaction transaction 
+          = new Transaction( doc ) )
+        {
+          transaction.Start( 
+            "Create New Room Boundary Lines" );
+
+          if( view == null )
+          {
+            view = doc.ActiveView;
+          }
+
+          SketchPlane sketchPlane = SketchPlane.Create( 
+            doc, view.GenLevel.Id );
+
+          ModelCurveArray modelCaRoomBoundaryLines 
+            = doc.Create.NewRoomBoundaryLines( 
+              sketchPlane, curves, view );
 
           //创建房间的坐标点
 
-          XYZ point = new XYZ( minX + 600 / 304.8, maxY - 600 / 304.8, 0 );
+          XYZ point = new XYZ( minX + 600 / 304.8, 
+            maxY - 600 / 304.8, 0 );
 
           //根据选中点，创建房间   当前视图的楼层doc.ActiveView.GenLevel
 
-          newRoom = doc.Create.NewRoom( view.GenLevel, new UV( point.X, point.Y ) );
+          newRoom = doc.Create.NewRoom( view.GenLevel, 
+            new UV( point.X, point.Y ) );
 
           if( newRoom == null )
           {
@@ -177,13 +212,19 @@ namespace BuildingCoder
             transaction.RollBack();
             return null;
           }
-          tag1 = doc.Create.NewRoomTag( new LinkElementId( newRoom.Id ), new UV( point.X, point.Y ), view.Id );
+          tag1 = doc.Create.NewRoomTag( 
+            new LinkElementId( newRoom.Id ), 
+            new UV( point.X, point.Y ), 
+            view.Id );
+
           transaction.Commit();
         }
 
-        //获取房间的墙体
+        //获取房间的墙体 -- Get the room wall
 
-        List<ElementId> elementIds = DetermineAdjacentElementLengthsAndWallAreas( doc, newRoom );
+        List<ElementId> elementIds 
+          = DetermineAdjacentElementLengthsAndWallAreas( 
+            doc, newRoom );
 
         group.RollBack(); // 撤销
 
