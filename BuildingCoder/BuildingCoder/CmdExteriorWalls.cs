fe35957a5a32c10144ed4a9a1dea9c5f556a20a5
Jeremy Tammik <jeremy.tammik@autodesk.com>
2018-05-23 13:56:05 +0200

    added skeleton for CmdExteriorWalls

---
+++ BuildingCoder/BuildingCoder/CmdExteriorWalls.cs
@@ -0,0 +1,34 @@
+﻿#region Header
+//
+// CmdExteriorWalls.cs - retrieve all parameter values for all elements of the given categories
+//
+// Copyright (C) 2018 Jeremy Tammik, Autodesk Inc. All rights reserved.
+//
+// Keywords: The Building Coder Revit API C# .NET add-in.
+//
+#endregion // Header
+
+#region Namespaces
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+#endregion // Namespaces
+
+namespace BuildingCoder
+{
+  [Transaction( TransactionMode.ReadOnly )]
+  class CmdExteriorWalls : IExternalCommand
+  {
+    public Result Execute(
+      ExternalCommandData commandData,
+      ref string message,
+      ElementSet elements )
+    {
+      UIApplication uiapp = commandData.Application;
+      UIDocument uidoc = uiapp.ActiveUIDocument;
+      Document doc = uidoc.Document;
+
+      return Result.Succeeded;
+    }
+  }
+}
