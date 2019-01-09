bc1335c8c5b1b9048bfa1c107e93390d39117ab4
Jeremy Tammik <jeremy.tammik@autodesk.com>
2019-01-09 10:48:19 +0100

    update copyright year to 2019

--- BuildingCoder/BuildingCoder/CmdExportIfc.cs
+++ BuildingCoder/BuildingCoder/CmdExportIfc.cs
@@ -2,7 +2,7 @@
 //
 // CmdExportIfc.cs - Export current view to IFC
 //
-// Copyright (C) 2018 Jeremy Tammik, Autodesk Inc. All rights reserved.
+// Copyright (C) 2018-2019 by Jeremy Tammik, Autodesk Inc. All rights reserved.
 //
 // Keywords: The Building Coder Revit API C# .NET add-in.
 //
