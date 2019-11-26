d32e4141749f8e856ab89b1519a29992a3dd579d
Jeremy Tammik <jeremy.tammik@autodesk.com>
2019-11-26 14:20:49 +0100

    whitespace

--- BuildingCoder/BuildingCoder/CmdElectricalLoad.cs
+++ BuildingCoder/BuildingCoder/CmdElectricalLoad.cs
@@ -1,4 +1,15 @@
-﻿using System.Collections.Generic;
+﻿#region Header
+//
+// CmdElectricalLoad.cs - React to elevation view creation
+//
+// Copyright (C) 2019 by Alexander Ignatovich and Jeremy Tammik, Autodesk Inc. All rights reserved.
+//
+// Keywords: The Building Coder Revit API C# .NET add-in.
+//
+#endregion // Header
+
+#region Namespaces
+using System.Collections.Generic;
 using System.Linq;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
@@ -6,120 +17,156 @@ using Autodesk.Revit.DB.Electrical;
 using Autodesk.Revit.Exceptions;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
+#endregion // Namespaces
 
 namespace BuildingCoder
 {
-    [Transaction(TransactionMode.Manual)]
-    public class CmdElectricalLoad : IExternalCommand
+  [Transaction( TransactionMode.Manual )]
+  public class CmdElectricalLoad : IExternalCommand
+  {
+    class ElectricalApparentLoad
     {
-        class ElectricalApparentLoad
-        {
-            public ElectricalApparentLoad(ElectricalSystemType electricalSystemType, int connectorId, double apparentLoad)
-            {
-                ElectricalSystemType = electricalSystemType;
+      public ElectricalApparentLoad( 
+        ElectricalSystemType electricalSystemType, 
+        int connectorId, 
+        double apparentLoad )
+      {
+        ElectricalSystemType = electricalSystemType;
+        ConnectorId = connectorId;
+        ApparentLoad = apparentLoad;
+      }
 
-                ConnectorId = connectorId;
+      public ElectricalSystemType ElectricalSystemType { get; }
 
-                ApparentLoad = apparentLoad;
-            }
+      public int ConnectorId { get; }
 
-            public ElectricalSystemType ElectricalSystemType { get; }
+      public double ApparentLoad { get; }
 
-            public int ConnectorId { get; }
-
-            public double ApparentLoad { get; }
-
-            public override string ToString() => $"{ElectricalSystemType}: {ConnectorId} - {ApparentLoad} V*A";
-        }
-
-        class ElectricalApparentLoadFactory
-        {
-            public IEnumerable<ElectricalApparentLoad> Create(FamilyInstance familyInstance)
-            {
-                return familyInstance
-                    .MEPModel
-                    .ConnectorManager
-                    .Connectors
-                    .Cast<Connector>()
-                    .Select(Create)
-                    .Where(x => x != null);
-            }
-
-            private static ElectricalApparentLoad Create(Connector connector)
-            {
-                if (connector.Domain != Domain.DomainElectrical)
-                    return null;
-                
-                var mepConnectorInfo = connector.GetMEPConnectorInfo() as MEPFamilyConnectorInfo;
-                
-                var parameterValue = mepConnectorInfo?.GetConnectorParameterValue(new ElementId(BuiltInParameter.RBS_ELEC_APPARENT_LOAD)) as DoubleParameterValue;
-
-                if (parameterValue == null)
-                    return null;
-
-                var load = UnitUtils.ConvertFromInternalUnits(parameterValue.Value, DisplayUnitType.DUT_VOLT_AMPERES);
-
-                return new ElectricalApparentLoad(connector.ElectricalSystemType, connector.Id, load);
-            }
-        }
-
-        class FamilyInstanceWithApparentLoadSelectionFilter : ISelectionFilter
-        {
-            private readonly ElectricalApparentLoadFactory electricalApparentLoadFactory;
-
-            public FamilyInstanceWithApparentLoadSelectionFilter(ElectricalApparentLoadFactory electricalApparentLoadFactory)
-            {
-                this.electricalApparentLoadFactory = electricalApparentLoadFactory;
-            }
-
-            public bool AllowElement(Element elem)
-            {
-                var familyInstance = elem as FamilyInstance;
-
-                if (familyInstance == null)
-                    return false;
-
-                return electricalApparentLoadFactory
-                    .Create(familyInstance)
-                    .Any();
-            }
-
-            public bool AllowReference(Reference reference, XYZ position) => false;
-        }
-
-        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
-        {
-            var app = commandData.Application;
-            var uidoc = app.ActiveUIDocument;
-
-            var familyInstance = SelectFamilyInstanceWithApparentLoad(uidoc);
-
-            if (familyInstance == null)
-                return Result.Cancelled;
-
-            var electricalApparentLoadFactory = new ElectricalApparentLoadFactory();
-
-            var apparentLoads = electricalApparentLoadFactory.Create(familyInstance);
-
-            TaskDialog.Show("dev", string.Join("\n", apparentLoads));
-
-            return Result.Succeeded;
-        }
-
-        private static FamilyInstance SelectFamilyInstanceWithApparentLoad(UIDocument uidoc)
-        {
-            var electricalApparentLoadFactory = new ElectricalApparentLoadFactory();
-
-            var selectionFilter = new FamilyInstanceWithApparentLoadSelectionFilter(electricalApparentLoadFactory);
-
-            try
-            {
-                return (FamilyInstance)uidoc.Document.GetElement(uidoc.Selection.PickObject(ObjectType.Element, selectionFilter));
-            }
-            catch (OperationCanceledException)
-            {
-                return null;
-            }
-        }
+      public override string ToString() => 
+        $"{ElectricalSystemType}: {ConnectorId} "
+        + "- {ApparentLoad} V*A";
     }
+
+    class ElectricalApparentLoadFactory
+    {
+      public IEnumerable<ElectricalApparentLoad> 
+        Create( FamilyInstance familyInstance )
+      {
+        return familyInstance.MEPModel
+          .ConnectorManager
+          .Connectors
+          .Cast<Connector>()
+          .Select( Create )
+          .Where( x => x != null );
+      }
+
+      private static ElectricalApparentLoad Create( 
+        Connector connector )
+      {
+        if( connector.Domain != Domain.DomainElectrical )
+          return null;
+
+        var mepConnectorInfo 
+          = connector.GetMEPConnectorInfo() 
+            as MEPFamilyConnectorInfo;
+
+        var parameterValue = mepConnectorInfo
+          ?.GetConnectorParameterValue( 
+            new ElementId( 
+              BuiltInParameter.RBS_ELEC_APPARENT_LOAD ) )
+                as DoubleParameterValue;
+
+        if( parameterValue == null )
+          return null;
+
+        var load = UnitUtils.ConvertFromInternalUnits( 
+          parameterValue.Value,
+          DisplayUnitType.DUT_VOLT_AMPERES );
+
+        return new ElectricalApparentLoad( 
+          connector.ElectricalSystemType, 
+          connector.Id, load );
+      }
+    }
+
+    class FamilyInstanceWithApparentLoadSelectionFilter 
+      : ISelectionFilter
+    {
+      private readonly ElectricalApparentLoadFactory 
+        electricalApparentLoadFactory;
+
+      public FamilyInstanceWithApparentLoadSelectionFilter( 
+        ElectricalApparentLoadFactory 
+          electricalApparentLoadFactory )
+      {
+        this.electricalApparentLoadFactory 
+          = electricalApparentLoadFactory;
+      }
+
+      public bool AllowElement( Element elem )
+      {
+        var familyInstance = elem as FamilyInstance;
+
+        if( familyInstance == null )
+          return false;
+
+        return electricalApparentLoadFactory
+          .Create( familyInstance )
+          .Any();
+      }
+
+      public bool AllowReference( Reference r, XYZ p ) 
+        => false;
+    }
+
+    public Result Execute( 
+      ExternalCommandData commandData, 
+      ref string message, 
+      ElementSet elements )
+    {
+      var app = commandData.Application;
+      var uidoc = app.ActiveUIDocument;
+
+      var familyInstance 
+        = SelectFamilyInstanceWithApparentLoad( 
+          uidoc );
+
+      if( familyInstance == null )
+        return Result.Cancelled;
+
+      var electricalApparentLoadFactory 
+        = new ElectricalApparentLoadFactory();
+
+      var apparentLoads = electricalApparentLoadFactory
+        .Create( familyInstance );
+
+      TaskDialog.Show( "CmdElectricalLoad", 
+        string.Join( "\n", apparentLoads ) );
+
+      return Result.Succeeded;
+    }
+
+    private static FamilyInstance 
+      SelectFamilyInstanceWithApparentLoad( 
+        UIDocument uidoc )
+    {
+      var electricalApparentLoadFactory 
+        = new ElectricalApparentLoadFactory();
+
+      var selectionFilter 
+        = new FamilyInstanceWithApparentLoadSelectionFilter( 
+          electricalApparentLoadFactory );
+
+      try
+      {
+        return (FamilyInstance) uidoc.Document.GetElement( 
+          uidoc.Selection.PickObject( ObjectType.Element, 
+            selectionFilter ) );
+      }
+      catch( OperationCanceledException )
+      {
+        return null;
+      }
+    }
+  }
 }
