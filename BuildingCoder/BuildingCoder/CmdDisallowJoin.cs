2ea429867cacf1f029ad3c1f9c8042e0042f496b
Jeremy Tammik <jeremy.tammik@autodesk.com>
2020-04-14 19:27:42 +0200

    implemented GetNoteBlockSymbolsLackingParameterNamed for https://forums.autodesk.com/t5/revit-api-forum/determine-if-a-parameter-exists-for-noteblockfamiles/m-p/9442331

--- BuildingCoder/BuildingCoder/CmdDisallowJoin.cs
+++ BuildingCoder/BuildingCoder/CmdDisallowJoin.cs
@@ -17,8 +17,6 @@ using Autodesk.Revit.ApplicationServices;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
-using Autodesk.Revit.UI.Selection;
-
 #endregion // Namespaces
 
 namespace BuildingCoder
