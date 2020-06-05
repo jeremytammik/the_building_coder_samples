8107deed6e0dd22ecfa54605be20c56d08da2a6d
Jeremy Tammik <jeremy.tammik@autodesk.com>
2020-06-05 13:54:11 +0200

    added cahnge element category material

--- BuildingCoder/BuildingCoder/CmdChangeElementColor.cs
+++ BuildingCoder/BuildingCoder/CmdChangeElementColor.cs
@@ -2,6 +2,8 @@
 //
 // CmdChangeElementColor.cs - Change element colour using OverrideGraphicSettings for active view
 //
+// Also change its category's material to a random material
+//
 // Copyright (C) 2020 by Jeremy Tammik, Autodesk Inc. All rights reserved.
 //
 // Keywords: The Building Coder Revit API C# .NET add-in.
@@ -13,6 +15,9 @@ using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 #endregion // Namespaces
 
 namespace BuildingCoder
@@ -20,6 +25,47 @@ namespace BuildingCoder
   [Transaction( TransactionMode.Manual )]
   public class CmdChangeElementColor : IExternalCommand
   {
+    void ChangeElementColor( Document doc, ElementId id )
+    {
+      Color color = new Color(
+        (byte) 200, (byte) 100, (byte) 100 );
+
+      OverrideGraphicSettings ogs = new OverrideGraphicSettings();
+      ogs.SetProjectionLineColor( color );
+
+      using( Transaction tx = new Transaction( doc ) )
+      {
+        tx.Start( "Change Element Color" );
+        doc.ActiveView.SetElementOverrides( id, ogs );
+        tx.Commit();
+      }
+    }
+
+    void ChangeElementMaterial( Document doc, ElementId id )
+    {
+      List<Material> materials = new List<Material>(
+        new FilteredElementCollector( doc )
+          .WhereElementIsNotElementType()
+          .OfClass( typeof( Material ) )
+          .ToElements()
+          .Cast<Material>() );
+
+      Random r = new Random();
+      int i = r.Next( materials.Count );
+
+      Element e = doc.GetElement( id );
+
+      if( null != e.Category )
+      {
+        using( Transaction tx = new Transaction( doc ) )
+        {
+          tx.Start( "Change Element Material" );
+          e.Category.Material = materials[ i ];
+          tx.Commit();
+        }
+      }
+    }
+
     public Result Execute(
       ExternalCommandData commandData,
       ref string message,
@@ -44,18 +90,10 @@ namespace BuildingCoder
         return Result.Cancelled;
       }
 
-      Color color = new Color(
-        (byte) 150, (byte) 200, (byte) 200 );
+      ChangeElementColor( doc, id );
 
-      OverrideGraphicSettings ogs = new OverrideGraphicSettings();
-      ogs.SetProjectionLineColor( color );
+      ChangeElementMaterial( doc, id );
 
-      using( Transaction tx = new Transaction( doc ) )
-      {
-        tx.Start( "Change Element Color" );
-        doc.ActiveView.SetElementOverrides( id, ogs );
-        tx.Commit();
-      }
       return Result.Succeeded;
     }
   }
