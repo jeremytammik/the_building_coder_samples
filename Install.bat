b5c5addb3808560a7cdf0688a28996e58671e3f3
Jeremy Tammik <jeremy.tammik@autodesk.com>
2024-04-08 09:45:17 +0200

    updated binary path in BcSamples.txt

---
+++ Install.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Install.bat -- copy add-in tbc samples manifest and BcSamples.txt to AddIns folder
+set p=C:\Users\jta\AppData\Roaming\Autodesk\Revit\Addins\2025
+copy BcSamples.txt %p
